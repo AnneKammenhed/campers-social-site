9df18c95bf6918987e03a54bd8dc8403f4826cc9
AnneKammenhed <anne.juriado@gmail.com>
2023-02-18 13:42:52 +0000

    add paths to navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import NavBar from './components/NavBar';
-import styles from './App.module.css'
+import styles from './App.module.css';
+import Container from "react-bootstrap/Container";
+import {Route, Switch} from "react-router-dom"
 
 function App() {
   return (
     <div className={styles.App}>
       <h1>Hello campers!</h1>
       <NavBar />
+      <Container className={styles.Main}>
+      <Switch>
+          <Route exact path="/" render={() => <h2>Home page</h2>} />
+          <Route exact path="/signin" render={() => <h2>Sign in</h2>} />
+          <Route exact path="/signup" render={() => <h2>Sign up</h2>} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
+
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
   return (
@@ -8,15 +9,15 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink to="/">
               <i className="fas fa-home"></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signin">
               <i className="fas fa-sign-in-alt"></i>Sign in
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signup">
               <i className="fas fa-user-plus"></i>Sign up
-            </Nav.Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
