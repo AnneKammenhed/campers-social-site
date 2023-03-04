782c588346bf92d9ed90b03d3e59c1f17cfa242c
AnneKammenhed <anne.juriado@gmail.com>
2023-03-04 10:45:50 +0000

    correct connection to API secret key

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://campers-api.herokuapp.com/";
+// axios.defaults.baseURL = "https://8000-annekammenhe-campersapi-tv64278kqhf.ws-eu89.gitpod.io";
+
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
